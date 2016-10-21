50c4c51afe282f7b0c38fe4f178ea21c191bf48a
Nathaniel Phillips <Nathaniel.D.Phillips.is@gmail.com>
2016-10-21 10:40:01 +0200

    Added version number to startup message

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 
 .onAttach <- function(libname, pkgname) {
-  packageStartupMessage("************\nWelcome to FFTrees")
+  packageStartupMessage("************\nWelcome to FFTrees 1.1.9")
   packageStartupMessage("Run FFTrees.guide() to see the main package guide")
   packageStartupMessage("Email me at Nathaniel.D.Phillips.is@gmail.com with questions, comments, or movie recommendations\n************")
 }
