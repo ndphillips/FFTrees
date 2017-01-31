8b089d2d1b078366788d3bb938337ba545a88056
Nathaniel Phillips <Nathaniel.D.Phillips.is@gmail.com>
2017-01-31 12:11:48 +0100

    minor bug fix

--- R/FFForest.R
+++ R/FFForest.R
@@ -224,7 +224,7 @@ for(stat.i in c("cues", "thresholds", "directions", "classes", "exits")) {
 
 # Train stats
 
-for(stat.i in c("n", "hi", "mi", "fa", "acc", "bacc", "dprime", "frugality", "mcpc")) {
+for(stat.i in c("n", "hi", "mi", "fa", "sens", "spec", "acc", "bacc", "dprime", "frugality", "mcpc")) {
 
 
   simulations[[paste0(stat.i, ".train")]] <- sapply(1:length(result.ls),
