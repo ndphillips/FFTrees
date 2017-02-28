65933968f82bc92dbd45de91c7a613f41cb49d9a
Nathaniel Phillips <Nathaniel.D.Phillips.is@gmail.com>
2017-02-28 17:25:54 +0100

    bug fixes

--- R/printFFForest_function.R
+++ R/printFFForest_function.R
@@ -27,20 +27,20 @@ summary.df <- data.frame("train" = c(
                                       round(mean(x$tree.sim$bacc.train), 2),
                                       round(mean(x$tree.sim$sens.train), 2),
                                       round(mean(x$tree.sim$spec.train), 2),
-                                      round(mean(x$tree.sim$mcpc.train), 2),
-                                      round(mean(x$tree.sim$frugality.train), 2)
+                                      round(mean(x$tree.sim$mcu.train), 2),
+                                      round(mean(x$tree.sim$pci.train), 2)
                                      ),
                          "test" = c(
                            round(mean(x$tree.sim$acc.test), 2),
                            round(mean(x$tree.sim$bacc.test), 2),
                            round(mean(x$tree.sim$sens.test), 2),
                            round(mean(x$tree.sim$spec.test), 2),
-                           round(mean(x$tree.sim$mcpc.test), 2),
-                           round(mean(x$tree.sim$frugality.test), 2)
+                           round(mean(x$tree.sim$mcu.test), 2),
+                           round(mean(x$tree.sim$pci.test), 2)
                          )
                          )
 
-row.names(summary.df) <- c("acc", "bacc", "sens", "spec", "mcpc", "frug")
+row.names(summary.df) <- c("acc", "bacc", "sens", "spec", "mcu", "pci")
 importance.text <- paste("Cue Importance:")
 
 
