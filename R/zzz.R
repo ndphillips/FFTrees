872dd832ca4ba3e2a5cea0ee13d9ed284cc4b028
Nathaniel Phillips <Nathaniel.D.Phillips.is@gmail.com>
2017-04-25 13:48:41 +0200

    package loading updates

--- R/zzz.R
+++ R/zzz.R
@@ -2,10 +2,10 @@
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage("FFTrees v1.2.2. FFTrees.guide() opens the package guide.")
   packageStartupMessage("Email: Nathaniel.D.Phillips.is@gmail.com")
-  packageStartupMessage("----o------")
-  packageStartupMessage("---/-\\-----")
-  packageStartupMessage("--x---o----")
-  packageStartupMessage("-----/-\\---")
-  packageStartupMessage("----x---x--")
+  packageStartupMessage("      o      ")
+  packageStartupMessage("     / \\     ")
+  packageStartupMessage("    x   o    ")
+  packageStartupMessage("       / \\   ")
+  packageStartupMessage("      x   x  ")
 
 }
