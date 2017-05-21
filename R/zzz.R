25851e94daa1082c15aed2b422f05f8ec38db145
Nathaniel Phillips <Nathaniel.D.Phillips.is@gmail.com>
2017-05-21 12:17:28 +0200

    Updated version number to 1.3.0

--- R/zzz.R
+++ R/zzz.R
@@ -5,6 +5,6 @@
   packageStartupMessage("    0   b  ")
   packageStartupMessage("       / \\   ")
   packageStartupMessage("      0   1  ")
-  packageStartupMessage("FFTrees v1.2.3. FFTrees.guide() opens the package guide.")
+  packageStartupMessage("FFTrees v1.3.0. FFTrees.guide() opens the package guide.")
   packageStartupMessage("Email: Nathaniel.D.Phillips.is@gmail.com")
 }
