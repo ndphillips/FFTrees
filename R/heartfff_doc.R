83faa32a7ac5a3cba7eb4dba84472a023c10f5e0
Nathaniel Phillips <Nathaniel.D.Phillips.is@gmail.com>
2017-06-21 15:57:11 +0200

    minor

---
+++ R/heartfff_doc.R
@@ -0,0 +1,9 @@
+#' An FFForest object trained on the heartdisease dataset.
+#'
+#'
+#' @format An FFForest object
+#' @details The code used to generate this object was \code{heart.fff <- FFForest(diagnosis ~., data = heartdisease, ntree = 100, train.p = .5, cpus = 4)}
+#'
+#'
+
+"heart.fff"
