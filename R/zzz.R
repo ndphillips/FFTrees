ac355637c5903fa5c7d52fe22aa4d24849b0263f
Nathaniel Phillips <Nathaniel.D.Phillips.is@gmail.com>
2017-07-27 19:53:39 +0200

    Updated citation links, updated package loading fft

--- R/zzz.R
+++ R/zzz.R
@@ -1,10 +1,10 @@
 
 .onAttach <- function(libname, pkgname) {
-  packageStartupMessage("   a      ")
+  packageStartupMessage("   O      ")
   packageStartupMessage("  / \\     ")
-  packageStartupMessage(" 0   b  ")
+  packageStartupMessage(" F   O  ")
   packageStartupMessage("    / \\   ")
-  packageStartupMessage("   0   1  ")
+  packageStartupMessage("   F   T  ")
   packageStartupMessage("FFTrees v1.3.3. Email: Nathaniel.D.Phillips.is@gmail.com")
   packageStartupMessage("FFTrees.guide() opens the package guide. Citation info at citation('FFTrees')")
 }
