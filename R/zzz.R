361c1ffb8a6f24b0e8387e119edb67a52c7f6e48
Nathaniel Phillips <Nathaniel.D.Phillips.is@gmail.com>
2017-11-01 21:51:29 +0100

    version 1.3.5

--- R/zzz.R
+++ R/zzz.R
@@ -5,6 +5,6 @@
   packageStartupMessage(" F   O  ")
   packageStartupMessage("    / \\   ")
   packageStartupMessage("   F   T  ")
-  packageStartupMessage("FFTrees v1.3.4. Email: Nathaniel.D.Phillips.is@gmail.com")
+  packageStartupMessage("FFTrees v1.3.5. Email: Nathaniel.D.Phillips.is@gmail.com")
   packageStartupMessage("FFTrees.guide() opens the package guide. Citation info at citation('FFTrees')")
 }
