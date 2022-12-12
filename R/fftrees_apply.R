446bb8c3501ff592d03494a4ea18d4682c39496e
Hansjörg Neth <hansneth@gmail.com>
2022-12-12 11:33:49 +0100

    simplify code (by introducing n_trees and tree_df as helper variables, rather than always looking up parts of x), note ToDo for using user-defined tree.definitions

--- R/fftrees_apply.R
+++ R/fftrees_apply.R
@@ -26,7 +26,7 @@
 #' @export
 
 fftrees_apply <- function(x,
-                          mydata = NULL,   # either "train" or "test"
+                          mydata = NULL,   # data type (either "train" or "test")
                           newdata = NULL,
                           allNA.pred = FALSE) {
 
@@ -64,12 +64,28 @@ fftrees_apply <- function(x,
   criterion_n <- length(criterion_v)
 
 
+  # ToDo: Allow swapping x$trees$definitions by tree.definitions: ------
+
+  # - verify that tree.definitions contains valid FFTs and fits to current data
+  # - replace x$trees$definitions by tree.definitions
+  # - update x$trees$n
+
+  # +++ here now
+
+
+  # Simplify (code below): ------
+
+  # Extract key parts from FFTrees object x:
+  n_trees <- x$trees$n
+  tree_df <- x$trees$definitions  # (definitions are df)
+
+
   # Setup outputs: ------
 
   #  1. [decisions_ls]: ----
   #     A list containing tibbles, with 1 row per case, and 1 column per tree:
 
-  decisions_ls <- lapply(1:x$trees$n, FUN = function(i) {
+  decisions_ls <- lapply(1:n_trees, FUN = function(i) {
 
     tibble::tibble(
       criterion = criterion_v,
@@ -83,28 +99,28 @@ fftrees_apply <- function(x,
 
   })
 
-  names(decisions_ls) <- paste0("tree_", 1:x$trees$n)
+  names(decisions_ls) <- paste0("tree_", 1:n_trees)
 
   #  2. [level_stats_ls]: ----
   #     A list with 1 element per tree, each containing cumulative level statistics:
 
-  level_stats_ls <- vector("list", length = x$trees$n)
+  level_stats_ls <- vector("list", length = n_trees)
 
 
   # LOOPs: ------
 
-  #  Loop over trees: ----
+  #  Loop 1 (over trees): ----
 
-  for (tree_i in 1:x$trees$n) {
+  for (tree_i in 1:n_trees) {
 
     # Extract definition of current tree:
-    cue_v   <- trimws(unlist(strsplit(x$trees$definitions$cues[tree_i], ";")))  # +++ here now +++: Added trimws()
-    class_v <- trimws(unlist(strsplit(x$trees$definitions$classes[tree_i], ";")))
-    exit_v  <- trimws(unlist(strsplit(x$trees$definitions$exits[tree_i], ";")))
-    threshold_v <- trimws(unlist(strsplit(x$trees$definitions$thresholds[tree_i], ";")))
-    direction_v <- trimws(unlist(strsplit(x$trees$definitions$directions[tree_i], ";")))
+    cue_v   <- trimws(unlist(strsplit(tree_df$cues[tree_i], ";")))  # +++ here now +++: Added trimws()
+    class_v <- trimws(unlist(strsplit(tree_df$classes[tree_i], ";")))
+    exit_v  <- trimws(unlist(strsplit(tree_df$exits[tree_i], ";")))
+    threshold_v <- trimws(unlist(strsplit(tree_df$thresholds[tree_i], ";")))
+    direction_v <- trimws(unlist(strsplit(tree_df$directions[tree_i], ";")))
 
-    level_n <- x$trees$definitions$nodes[tree_i]
+    level_n <- tree_df$nodes[tree_i]
 
     decisions_df <- decisions_ls[[tree_i]]
 
@@ -135,7 +151,7 @@ fftrees_apply <- function(x,
       stringsAsFactors = FALSE
     )
 
-    # Define critical stats:  +++ here now +++
+    # Define critical stats:
     critical_stats_v <- c(
       # frequencies:
       "n", "hi", "fa", "mi", "cr",
@@ -152,10 +168,12 @@ fftrees_apply <- function(x,
     level_stats_i[critical_stats_v] <- NA
     level_stats_i$cost_cues <- NA
 
-    # Loop over levels: ----
+
+    # Loop 2 (over levels): ----
+
     for (level_i in 1:level_n) {
 
-      # Get definitions for current level:
+      # Get tree definition at current level:
       cue_i <- cue_v[level_i]
       class_i <- class_v[level_i]
       direction_i <- direction_v[level_i]
@@ -222,8 +240,8 @@ fftrees_apply <- function(x,
       decisions_df$levelout[classify_now] <- level_i
       decisions_df$cost_cue[classify_now] <- cost_cue_level_cum[level_i]
 
-      decisions_df$cost_decision[decisions_df$criterion == TRUE & decisions_df$decision == TRUE]   <- x$params$cost.outcomes$hi
-      decisions_df$cost_decision[decisions_df$criterion == TRUE & decisions_df$decision == FALSE]  <- x$params$cost.outcomes$mi
+      decisions_df$cost_decision[decisions_df$criterion == TRUE  & decisions_df$decision == TRUE]  <- x$params$cost.outcomes$hi
+      decisions_df$cost_decision[decisions_df$criterion == TRUE  & decisions_df$decision == FALSE] <- x$params$cost.outcomes$mi
       decisions_df$cost_decision[decisions_df$criterion == FALSE & decisions_df$decision == TRUE]  <- x$params$cost.outcomes$fa
       decisions_df$cost_decision[decisions_df$criterion == FALSE & decisions_df$decision == FALSE] <- x$params$cost.outcomes$cr
 
@@ -267,26 +285,30 @@ fftrees_apply <- function(x,
 
   helper <- paste(level_stats$tree, level_stats$level, sep = ".")
   maxlevs <- paste(rownames(tapply(level_stats$level, level_stats$tree, FUN = which.max)), tapply(level_stats$level, level_stats$tree, FUN = which.max), sep = ".")
-  tree_stats <- cbind(x$trees$definitions[, c("tree")], level_stats[helper %in% maxlevs, c(critical_stats_v, "cost_cues", "cost")])
+  tree_stats <- cbind(tree_df[, c("tree")], level_stats[helper %in% maxlevs, c(critical_stats_v, "cost_cues", "cost")])
   names(tree_stats)[1] <- "tree"
   rownames(tree_stats) <- 1:nrow(tree_stats)
 
+
   # Calculate pci and mcu: ----
 
-  for (tree_i in 1:x$trees$n) {
-    max.lookups <- nrow(data) * length(x$cue_names)
-    n.lookups <- sum(decisions_ls[[tree_i]]$levelout)
+  for (tree_i in 1:n_trees) {
 
-    tree_stats$pci[tree_i] <- 1 - n.lookups / max.lookups
+    max_lookups <- nrow(data) * length(x$cue_names)
+    n_lookups   <- sum(decisions_ls[[tree_i]]$levelout)
+
+    tree_stats$pci[tree_i] <- 1 - n_lookups / max_lookups
     tree_stats$mcu[tree_i] <- mean(decisions_ls[[tree_i]]$levelout)
 
   }
+
 
   # Add results to trees in x: ----
 
   x$trees$stats[[mydata]]       <- tibble::as_tibble(tree_stats)
   x$trees$level_stats[[mydata]] <- tibble::as_tibble(level_stats)
   x$trees$decisions[[mydata]]   <- decisions_ls
+
 
   # Set best tree values:
   if (mydata == "train"){
