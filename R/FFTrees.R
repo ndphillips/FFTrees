1e9d52b93cff7a9a5fbe73043d24f4de545e69d8
Hansjörg Neth <hansneth@gmail.com>
2023-02-24 21:02:00 +0100

    handle NA cases in character and factor predictor variables

--- R/FFTrees.R
+++ R/FFTrees.R
@@ -343,17 +343,71 @@ FFTrees <- function(formula = NULL,
 
   # C. Handle data: ------
 
+  # NA cases: ----
 
-  # Handle NA cases: ----
+  if (any(is.na(data))){
 
-  # Convert factor NA values to a missing <NA> factor level:
-  data <- data %>%
-    dplyr::mutate_if(is.factor, addNA) %>%
-    dplyr::mutate_if(is.character, addNA)
+    # # Convert factor NA values to a missing <NA> factor level:
+    # data <- data %>%
+    #  dplyr::mutate_if(is.factor, addNA) %>%
+    #  dplyr::mutate_if(is.character, addNA)
+
+    ix_crit <- names(data) == criterion_name
+    ix_pred <- names(data) != criterion_name
+
+    ix_chr <- sapply(my_data, is.character)
+    ix_fct <- sapply(my_data, is.factor)
+    ix_num <- sapply(my_data, is.numeric)
+
+    ix_pred_chr <- ix_pred & ix_chr
+    ix_pred_fct <- ix_pred & ix_fct
+    ix_pred_num <- ix_pred & ix_num
+
+    if (any(is.na(data[ix_pred_chr]))){
+
+      # Replace NA values:
+      data[ix_pred_chr] <- data[ix_pred_chr] %>%
+        dplyr::mutate_if(is.character, addNA)
+
+      # Provide user feedback:
+      cli::cli_alert_info("Converted NA cases in character predictors to <NA>.")
+
+    }
+
+    if (any(is.na(data[ix_pred_fct]))){
+
+      # Replace NA values:
+      data[ix_pred_fct] <- data[ix_pred_fct] %>%
+        dplyr::mutate_if(is.factor, addNA)
+
+      # Provide user feedback:
+      cli::cli_alert_info("Converted NA cases in factor predictors to <NA> level.")
+
+    }
+
+    # +++ here now +++
+
+    if (any(is.na(data[ix_pred_num]))){
+
+      # ToDo: What to do about NA values in numeric variables?
+
+      # Provide user feedback:
+      cli::cli_alert_warning("There are NA cases in numeric predictors.")
+
+    }
+
+    if (any(is.na(data[ix_crit]))){
+
+      # ToDo: What to do about NA values in criterion?
+
+      # Provide user feedback:
+      cli::cli_alert_warning("There are NA values in the criterion variable.")
+
+    }
 
   # print(data)  # 4debugging
 
-  # +++ here now +++
+  } # (any(is.na(data))).
 
 
   # Split training / test data: ----
